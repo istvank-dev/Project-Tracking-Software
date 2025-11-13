d048fcd0d57de224c5dbfce3b886022d867ecae9
István <111610289+istvank-dev@users.noreply.github.com>
2025-11-13 12:53:05 +0100

    sqlite

--- ProjectTrackingSoftware.Server/Program.cs
+++ ProjectTrackingSoftware.Server/Program.cs
@@ -1,6 +1,19 @@
+// At the top, add using statements
+using Microsoft.EntityFrameworkCore;
+using ProjectTrackingSoftware.Server.Data;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "DataSource=app.db;Cache=Shared";
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddAuthorization(); // You already have this
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -20,6 +33,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// This maps all the identity endpoints like /register, /login, etc.
+app.MapIdentityApi<IdentityUser>();
 
 app.MapControllers();
 
